10f95a2dbd0a5ca5f797f9379d3938307211b224
Andres Rodriguez <andresrc@gmail.com>
2015-08-24 23:07:24 +0200

    Errors as exceptions

--- bootstrap.php
+++ bootstrap.php
@@ -1,8 +1,13 @@
 <?php
 
 require_once __DIR__.'/vendor/autoload.php';
+require_once __DIR__.'/error_handler.php';
 require_once __DIR__.'/config.php';
 
 use Derquinse\PhpAS\Module as M;
 
+// Set error handler
+set_error_handler('exception_error_handler');
+
+// Instantiate modules
 $module = new M\AddressModule($moduleConfig);
---
+++ error_handler.php
@@ -0,0 +1,11 @@
+<?php
+
+/** Error handler converting errors to exceptions. */
+function exception_error_handler($severity, $message, $file, $line)
+{
+    if (!(error_reporting() & $severity)) {
+        // This error code is not included in error_reporting
+        return;
+    }
+    throw new ErrorException($message, 0, $severity, $file, $line);
+}
